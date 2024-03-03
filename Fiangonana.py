670157a1742c112fa80bdf478cd3fad96c271052
Angelo RABEARISON <151778901+rAnjeel@users.noreply.github.com>
2024-03-03 09:39:02 +0300

    Fonction pour creer un view_caisse(avec numero dimanche annee et mois) Fonction get_liste_caisse
    Signed-off-by: Angelo RABEARISON <151778901+rAnjeel@users.noreply.github.com>

--- Fiangonana.py
+++ Fiangonana.py
@@ -1,5 +1,6 @@
 import pyodbc
 
+from Caisse import Caisse
 from SqlConnection import SqlConnection
 
 class Fiangonana:
@@ -41,8 +42,56 @@ class Fiangonana:
         finally:
             connection.close()
 
+    @staticmethod
+    def create_view_caisse(year1, year2):
+        try:
+            conn = SqlConnection('DESKTOP-RCL8G7D\SQLEXPRESS', 'Eglise', 'sa', 'rabearison')
+            conn.connect()
+            cursor = conn.connection.cursor()
 
- # Exemple d'utilisation :
+            cursor.execute(
+                "IF EXISTS (SELECT * FROM sys.views WHERE name = 'Vue_Caisse') BEGIN DROP VIEW Vue_Caisse; END")
+
+            cursor.execute(f"""
+                    CREATE VIEW Vue_Caisse AS
+                    SELECT
+                        dateInsertion AS Date,
+                        YEAR(dateInsertion) AS Annee,
+                        DENSE_RANK() OVER (ORDER BY DATEPART(WEEK, dateInsertion)) AS Numero_Dimanche_Annee,
+                        DENSE_RANK() OVER (PARTITION BY YEAR(dateInsertion), MONTH(dateInsertion) ORDER BY DATEPART(WEEK, dateInsertion)) AS Numero_Dimanche_Mois,
+                        montant AS Montant
+                    FROM caisse
+                    WHERE YEAR(dateInsertion) = {year1} OR YEAR(dateInsertion) = {year2}
+                """)
+
+            conn.connection.commit()
+            print("La vue a été créée ou remplacée avec succès.")
+
+        except pyodbc.Error as e:
+            print(f"Erreur lors de la création ou du remplacement de la vue : {e}")
+
+        finally:
+            if conn:
+                conn.close()
+
+    def get_liste_caisse(idFiangonana):
+        try:
+            connection = SqlConnection('DESKTOP-RCL8G7D\SQLEXPRESS', 'Eglise', 'sa', 'rabearison')
+            connection.connect()
+            cursor = connection.connection.cursor()
+            cursor.execute("SELECT * FROM caisse WHERE idFiangonana=?", (idFiangonana,))
+            rows = cursor.fetchall()
+            caisses = []
+            for row in rows:
+                caisses.append(Caisse(*row))
+            return caisses
+        except pyodbc.Error as e:
+            print(f"Erreur lors de la récupération de la liste des caisses : {e}")
+        finally:
+            connection.close()
+
+# Exemple d'utilisation :
 # Création d'un nouveau Mpiangona
-liste = Fiangonana.get_liste_croyants(1)
-print(liste)
+liste = Fiangonana.get_liste_caisse(1)
+for caisse in liste:
+    print(caisse.idcaisse, caisse.idFiangonana, caisse.montant, caisse.date)
--- Main.py
+++ Main.py
@@ -1,2 +1,21 @@
-# TEST TEST
-SLT THOMIS
+from datetime import datetime, timedelta
+
+def get_sundays_in_2023():
+    sundays = []
+    year = 2023
+    start_date = datetime(year, 1, 1)  # Premier jour de l'année 2023
+
+    # Trouver le premier dimanche
+    start_date += timedelta(days=(6 - start_date.weekday()))
+
+    # Parcourir toutes les semaines de l'année
+    while start_date.year == year:
+        sundays.append(start_date)
+        start_date += timedelta(weeks=1)
+
+    return sundays
+
+# Exemple d'utilisation :
+sundays_2023 = get_sundays_in_2023()
+for sunday in sundays_2023:
+    print(sunday.strftime('%Y-%m-%d'))
